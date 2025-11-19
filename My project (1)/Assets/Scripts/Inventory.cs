1ef98ba513301e6e940323447461c41d303bd957
kms060616 <2025137021rlaalstjr@g.shingu.ac.kr>
2025-11-19 20:34:10 +0900

    [feat]인벤토리추가

--- My project (1)/Assets/Scripts/Inventory.cs
+++ My project (1)/Assets/Scripts/Inventory.cs
@@ -6,24 +6,38 @@ public class Inventory : MonoBehaviour
 {
     public Dictionary<BlockType, int> items = new();
 
+
+    [SerializeField] InventoryUI inventoryUI;
+
     public void Add(BlockType type, int count = 1)
     {
         if (!items.ContainsKey(type)) items[type] = 0;
         items[type] += count;
         Debug.Log($"[Inventory] + {count} {type} (รั {items[type]})");
+
+        if (inventoryUI != null)
+            inventoryUI.UpdateInventory(this);
     }
 
     public bool Consume(BlockType type, int count = 1)
     {
         if (!items.TryGetValue(type, out var have) || have < count) return false;
         items[type] = have - count;
-        Debug.Log($"[Inventory] -{count} {type} (รั {items[type]}");
+        Debug.Log($"[Inventory] -{count} {type} (รั {items[type]})");
+
+        if (inventoryUI != null)
+            inventoryUI.UpdateInventory(this);
+
         return true;
+
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Add(BlockType.Dirt, 5);
+        Add(BlockType.Grass, 3);
+        Add(BlockType.Water, 1);
     }
 
     // Update is called once per frame
--- My project (1)/Assets/Scripts/InventoryUI.cs
+++ My project (1)/Assets/Scripts/InventoryUI.cs
@@ -2,11 +2,14 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class InventoryUI : MonoBehaviour
 {
     public Sprite Dirt;
-    public Sprite Garss;
+    public Sprite Grass;
+    public Sprite Water;
     public List<Transform> Slot = new List<Transform>();
     public GameObject SlotItem;
     List<GameObject> items = new List<GameObject>();
@@ -22,6 +25,10 @@ public class InventoryUI : MonoBehaviour
         int idx = 0;
         foreach (var item in myInven.items)
         {
+
+            if (idx >= Slot.Count)
+                break;
+
             var go = Instantiate(SlotItem, Slot[idx].transform);
             go.transform.localPosition = Vector3.zero;
             SlotItemPrefab sltem = go.GetComponent<SlotItemPrefab>();
@@ -30,13 +37,25 @@ public class InventoryUI : MonoBehaviour
             switch (item.Key)
             {
                 case BlockType.Dirt:
+                    sltem.itemImage.sprite = Dirt;
+                    break;
 
                 case BlockType.Water:
-                case BlockType.Grass:
-
+                    sltem.itemImage.sprite = Water;
                     break;
+                case BlockType.Grass:
+                    sltem.itemImage.sprite = Grass;
+                    break;
+
+
+            }
+
+            if (sltem.itemText != null)
+            {
+                sltem.itemText.text = item.Value.ToString();
             }
             idx++;
+
         }
     }
     // Start is called before the first frame update
